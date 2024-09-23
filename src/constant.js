d11d9bc0c9c1909123963f6cadb0c85257dbe15f
kim <perfumeplaylist.gmail.com>
2024-09-23 19:50:23 +0900

    feat: 상수 정의

---
+++ src/constant.js
@@ -0,0 +1,14 @@
+const CONSTANT = {
+  NUMBER: {
+    LIMIT_COUNT: 5,
+    MAX_NUMBER: 50,
+  },
+  MESSAGE: {
+    UP: "업",
+    DOWN: "다운",
+    YES: "yes",
+    NO: "no",
+  },
+};
+
+export default CONSTANT;
