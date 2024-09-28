9753e2cb78e814cf9aa72e1c2b76d8362a4cb10d
kim <perfumeplaylist.gmail.com>
2024-09-28 13:20:32 +0900

    refactor: Controller 활용을 위한 정합성 로직 분리

--- src/Model.js
+++ src/Model.js
@@ -5,8 +5,8 @@ import validation from "./validation.js";
 export default class Model {
   constructor() {
     this.count = 0;
-    this.prevValue = [];
     this.randomNumber = 0;
+    this.prevValue = [];
     this.isReStart = true;
     this.inputValue = null;
     this.game = null;
@@ -32,27 +32,34 @@ export default class Model {
     this.inputValue = value;
   }
 
+  validateNumberInput(number) {
+    if (validation.main(CONSTANT.ID.NUMBER, number, this.prevValue)) {
+      this.sameErrorMessage(number);
+    }
+    this.#updateInputValue(number);
+  }
+
   reTryGame(retryValue) {
-    const isRetry = validation.main("retry", retryValue);
+    const isRetry = validation.main(CONSTANT.ID.RETRY, retryValue);
     this.#updateIsReStart(isRetry);
     !isRetry && this.reTryErrorMessage(retryValue);
   }
 
   reTryErrorMessage(inputValue) {
     if (!validation.validateInput(inputValue, CONSTANT.MESSAGE.NO)) {
-      throw new CustomError("retry", "입력 형식이 잘못되었습니다.");
+      throw new CustomError(CONSTANT.ID.RETRY, "입력 형식이 잘못되었습니다.");
     }
   }
 
   sameErrorMessage(inputValue) {
     if (validation.sameValidation(inputValue, this.prevValue)) {
       throw new CustomError(
-        "isSame",
+        CONSTANT.ID.NUMBER,
         "동일한 값을 입력하였습니다.다시 입력해주세요"
       );
     } else {
       throw new CustomError(
-        "isSame",
+        CONSTANT.ID.NUMBER,
         "입력 형식이 잘못되었습니다.다시 입력해주세요"
       );
     }
@@ -61,33 +68,25 @@ export default class Model {
   async main() {
     while (this.count < CONSTANT.NUMBER.LIMIT_COUNT) {
       try {
-        const number = await this.game.askNumber();
+        await this.game.askNumber();
 
-        // 여기서는 같은 수가 있는지 확인
-
-        if (validation.main("number", number, this.prevValue)) {
-          this.sameErrorMessage(number);
-        }
-
-        this.#updateInputValue(number);
-        this.#updatePrevValue([...this.prevValue, number]);
+        this.#updatePrevValue([...this.prevValue, this.inputValue]);
         this.#updateCount(this.count + 1);
 
         if (validation.isSameValue(this.inputValue, this.randomNumber)) {
-          const retryValue = await this.game.success();
-          this.reTryGame(retryValue);
+          await this.game.success();
           return;
         }
 
         this.game.progress();
       } catch (error) {
-        if (error.errorName === "retry") throw new Error(error.message);
+        if (error.errorName === CONSTANT.ID.RETRY)
+          throw new CustomError(error.message);
         this.game.errorMessage(error.message);
         continue;
       }
     }
-    const retryValue = await this.game.failure();
-    this.reTryGame(retryValue);
+    await this.game.failure();
     return;
   }
 
