532f68bc1e42a00a7271ef57b5bd2e370cf59288
kim <perfumeplaylist.gmail.com>
2024-09-30 23:11:37 +0900

    refactor: controller 부분 상황 처리 및 전체적인 흐름 작성

--- src/game/GameController.js
+++ src/game/GameController.js
@@ -1,3 +1,4 @@
+import CustomError from "../CustomError.js";
 import Model from "./GameLogicModel.js";
 import View from "./GameView.js";
 
@@ -5,30 +6,6 @@ export default class GameController {
   constructor(model = new Model(), view = new View()) {
     this.model = model;
     this.view = view;
-  }
-
-  async askNumber() {
-    const number = parseInt(await this.view.input(`숫자 입력: `), 10);
-    this.model.validateNumberInput(number);
-  }
-
-  async askIsRetryGame() {
-    const reTryValue = await this.view.input(
-      "게임을 다시 시작하시겠습니까? (yes/no): "
-    );
-    this.model.reTryGame(reTryValue);
-  }
-
-  async success() {
-    const { count } = this.model;
-    this.view.success(count);
-    return await this.askIsRetryGame();
-  }
-
-  async failure() {
-    const { randomNumber } = this.model;
-    this.view.failure(randomNumber);
-    return await this.askIsRetryGame();
   }
 
   progress() {
@@ -40,12 +17,85 @@ export default class GameController {
     this.view.errorMessage(errorMessage);
   }
 
-  start() {
-    this.view.start();
-    this.model.reset();
-  }
-
   end() {
     this.view.end();
+  }
+
+  async askNumber() {
+    const number = parseInt(await this.view.input(`숫자 입력: `), 10);
+    const {
+      isError,
+      error: { name, message },
+    } = this.model.checkAndValidateInput(number);
+    if (isError) {
+      throw new CustomError(name, message);
+    } else {
+      this.model.updateSuccess(number);
+    }
+  }
+
+  async askIsRetryGame() {
+    const reTryValue = await this.view.input(
+      "게임을 다시 시작하시겠습니까? (yes/no): "
+    );
+    const isRetry = this.model.validateRetry(reTryValue);
+    this.model.updateIsReStart(isRetry);
+    if (!isRetry) {
+      const { errorName, errorMessage } =
+        this.model.reTryErrorMessage(reTryValue);
+      throw new CustomError(errorName, errorMessage);
+    }
+  }
+
+  async success() {
+    const { count } = this.model;
+    this.view.success(count);
+    await this.askIsRetryGame();
+  }
+
+  async exceedProcess() {
+    const { randomNumber } = this.model;
+    this.view.failure(randomNumber);
+    await this.askIsRetryGame();
+  }
+
+  successProcess() {
+    const isSuccess = this.model.checkSuccess();
+    return isSuccess;
+  }
+
+  async main() {
+    while (this.model.isDoingGame()) {
+      try {
+        await this.askNumber();
+        const isSuccess = this.successProcess();
+
+        if (isSuccess) {
+          await this.success();
+          return;
+        }
+
+        this.progress();
+      } catch (error) {
+        this.model.isThrowErrorMessage(error);
+        this.errorMessage(error.message);
+        continue;
+      }
+    }
+    await this.exceedProcess();
+    return;
+  }
+
+  async start() {
+    while (this.model.isReStart) {
+      try {
+        this.model.reset();
+        this.view.start();
+        await this.main();
+      } catch (error) {
+        this.errorMessage(error.message);
+      }
+    }
+    this.end();
   }
 }
